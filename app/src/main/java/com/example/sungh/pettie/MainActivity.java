6cf5a27bc39807a866f9ce8613e9ed8b73b2918b
Silvia <silvia0223y@gmail.com>
2017-04-28 15:52:42 +0800

    ButtonBar initialization

--- app/src/main/java/com/example/sungh/pettie/MainActivity.java
+++ app/src/main/java/com/example/sungh/pettie/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.sungh.pettie;
 
+import android.support.annotation.IdRes;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.content.res.Configuration;
@@ -16,6 +17,9 @@ import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.Toast;
+
+import com.roughike.bottombar.BottomBar;
+import com.roughike.bottombar.OnTabSelectListener;
 
 
 public class MainActivity extends AppCompatActivity {
@@ -36,6 +40,14 @@ public class MainActivity extends AppCompatActivity {
 
         addDrawerItems();
         setupDrawer();
+
+        BottomBar bottomBar = (BottomBar) findViewById(R.id.bottomBar);
+        bottomBar.setOnTabSelectListener(new OnTabSelectListener() {
+            @Override
+            public void onTabSelected(@IdRes int tabId) {
+
+            }
+        });
 
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         getSupportActionBar().setHomeButtonEnabled(true);
