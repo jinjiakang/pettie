4ca25a9707eec9a17bc0a27f39bb83de9ff8d062
Silvia <silvia0223y@gmail.com>
2017-05-10 21:02:25 +0800

    change add_post.xml layout

--- app/src/main/java/com/example/sungh/pettie/AddFragment.java
+++ app/src/main/java/com/example/sungh/pettie/AddFragment.java
@@ -40,6 +40,6 @@ public class AddFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_add, container, false);
+        return inflater.inflate(R.layout.add_post, container, false);
     }
 }
