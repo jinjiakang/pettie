fc8082356ac7048857142bfab8c4ec696bd81390
eric kang <sunghafans22@gmail.com>
2017-08-25 22:16:09 +0800

    add branch pettie2 ,create ForumActivity ansd resources

--- app/src/main/java/com/example/sungh/pettie/Home/HomeActivity.java
+++ app/src/main/java/com/example/sungh/pettie/Home/HomeActivity.java
@@ -1,19 +1,13 @@
 package com.example.sungh.pettie.Home;
 
-import android.app.Fragment;
-import android.app.FragmentManager;
 import android.content.Intent;
-import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
-import com.example.sungh.pettie.Add.AddActivity;
+import com.example.sungh.pettie.Main.ForumActivity;
 import com.example.sungh.pettie.Main.MainActivity;
-import com.example.sungh.pettie.Map.PetmapFragment;
-import com.example.sungh.pettie.Message.MessageFragment;
-import com.example.sungh.pettie.News.NewsViewpager.AdoptionViewpager;
 import com.example.sungh.pettie.R;
 
 public class HomeActivity extends AppCompatActivity {
@@ -30,8 +24,9 @@ public class HomeActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent();
-                intent.setClass(HomeActivity.this  , MainActivity.class);
+                intent.setClass(HomeActivity.this  , ForumActivity.class);
                 startActivity(intent);
+
             }
 
         });
@@ -39,7 +34,9 @@ public class HomeActivity extends AppCompatActivity {
         AdoptionBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-               // 之後做連結 會分開
+                Intent intent = new Intent();
+                intent.setClass(HomeActivity.this  , MainActivity.class);
+                startActivity(intent);
             }
 
         });
---
+++ app/src/main/java/com/example/sungh/pettie/Main/ForumActivity.java
@@ -0,0 +1,185 @@
+package com.example.sungh.pettie.Main;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.Toolbar;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.example.sungh.pettie.Add.AddActivity;
+import com.example.sungh.pettie.R;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ForumActivity extends AppCompatActivity
+        implements NavigationView.OnNavigationItemSelectedListener {
+    private MyAdapter mAdapter;
+    private RecyclerView mRecyclerView;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_forum);
+
+        // 實作toolbar
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        //實作 fab
+        final FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent();
+                intent.setClass(ForumActivity.this  ,AddActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        toggle.syncState();
+
+        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
+
+
+        // 實作RecyclerView
+        ArrayList<String> myDataset = new ArrayList<>();
+        for(int i = 0; i < 10; i++){
+            myDataset.add(Integer.toString(i));
+        }
+
+        mAdapter = new MyAdapter(myDataset);
+        mRecyclerView = (RecyclerView) findViewById(R.id.list_view);
+        final LinearLayoutManager layoutManager = new LinearLayoutManager(this);
+        layoutManager.setOrientation(LinearLayoutManager.VERTICAL);
+        mRecyclerView.setLayoutManager(layoutManager);
+        mRecyclerView.setAdapter(mAdapter);
+
+        // 判斷 fab 隱藏時機
+        mRecyclerView.addOnScrollListener(new RecyclerView.OnScrollListener()
+        {
+            @Override
+            public void onScrolled(RecyclerView recyclerView, int dx, int dy) {
+                if(dy > 0){
+                    fab.hide();
+                } else{
+                    fab.show();
+                }
+
+                super.onScrolled(recyclerView, dx, dy);
+            }
+
+        });
+    }
+
+    public class MyAdapter extends RecyclerView.Adapter<MyAdapter.ViewHolder> {
+        private List<String> mData;
+
+        public class ViewHolder extends RecyclerView.ViewHolder {
+            public TextView mTextView;
+            public ViewHolder(View v) {
+                super(v);
+                mTextView = (TextView) v.findViewById(R.id.text_Info);
+            }
+        }
+
+        public MyAdapter(List<String> data) {
+            mData = data;
+        }
+
+        @Override
+        public MyAdapter.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+            View v = LayoutInflater.from(parent.getContext())
+                    .inflate(R.layout.item, parent, false);
+            ViewHolder vh = new ViewHolder(v);
+            return vh;
+        }
+
+        @Override
+        public void onBindViewHolder(ViewHolder holder, final int position) {
+            holder.mTextView.setText(mData.get(position));
+        }
+              ;
+        @Override
+        public int getItemCount() {
+            return mData.size();
+        }
+
+
+
+    }
+
+
+
+
+    @Override
+    public void onBackPressed() {
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.forum, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    @SuppressWarnings("StatementWithEmptyBody")
+    @Override
+    public boolean onNavigationItemSelected(MenuItem item) {
+        // Handle navigation view item clicks here.
+        int id = item.getItemId();
+
+        if (id == R.id.nav_camera) {
+            // Handle the camera action
+        } else if (id == R.id.nav_gallery) {
+
+        } else if (id == R.id.nav_slideshow) {
+
+        } else if (id == R.id.nav_manage) {
+
+        } else if (id == R.id.nav_share) {
+
+        } else if (id == R.id.nav_send) {
+
+        }
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
+    }
+}
