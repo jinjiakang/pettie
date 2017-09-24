02c244e3f52a0ccd24f140bc5dbe57de3b41d742
Silvia <silvia0223y@gmail.com>
2017-09-25 02:12:13 +0800

    homepage gridview created.

---
+++ app/src/main/java/com/example/sungh/pettie/Main/AdoptionActivity.java
@@ -0,0 +1,34 @@
+//package com.example.sungh.pettie.Main;
+//
+//
+//import android.app.Activity;
+//import android.os.Bundle;
+//import android.support.annotation.Nullable;
+//import android.support.v7.app.AppCompatActivity;
+//import android.view.View;
+//import android.widget.AdapterView;
+//import android.widget.GridView;
+//import android.widget.SimpleAdapter;
+//import android.widget.Toast;
+//
+//import com.example.sungh.pettie.R;
+//
+//import java.util.ArrayList;
+//import java.util.HashMap;
+//import java.util.List;
+//import java.util.Map;
+//
+//public class AdoptionActivity extends AppCompatActivity {
+//
+//
+//
+//    @Override
+//    protected void onCreate(@Nullable Bundle savedInstanceState) {
+//        super.onCreate(savedInstanceState);
+//        setContentView(R.layout.);
+//
+//
+
+//
+//    }
+//}
